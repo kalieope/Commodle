a64189293a45a4283e0360985159f958b47dfc38
kalieope <113713658+kalieope@users.noreply.github.com>
2024-10-02 19:36:56 +0000

    Added placeholder pages

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,6 +5,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginSignup from './Components/LoginSignup/LoginSignup.jsx';
 import HomePage from './HomePage';
 import AboutPage from './AboutPage';
+import FavoritesPage from  './FavoritesPage.js';
+import ReviewPage from './ReviewPage.js';
 
 
 const App = () => {
@@ -14,11 +16,15 @@ const App = () => {
         <Route path="/" element={<LoginSignup />} />
         <Route path="/home" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+        <Route path="/reviews" element={<ReviewPage />} />
       </Routes>
       <div>
         <Link to="/">Login</Link>
         <Link to="/home">Home</Link>
         <Link to="/about">About</Link>
+        <Link to="/favorites">Favorites</Link>
+        <Link to ="/reviews">Reviews</Link>
       </div>
 
       {/* ... Routes component */}
