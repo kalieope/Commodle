a337cd27faf57cd94e3b85da2d84863f6b87ab68
Jacob Yee <Jacobdy18@hotmail.com>
2024-10-07 14:26:41 -0500

    maps

--- my-app/src/HomePage.js
+++ my-app/src/HomePage.js
@@ -1,36 +1,20 @@
-// HomePage.js
-import React from 'react';
-import { GoogleMap, LoadScript } from '@react-google-maps/api'
+import React, { useState, useEffect } from 'react';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 
-const mapContainerStyle = {
-    width: '75%',
-    height: '500px'
-}
+const MapComponent = () => {
+  const [center, setCenter] = useState({ lat: 40.7128, lng: -74.0060 }); // Default center (e.g., New York)
 
-const center = {
-    lat: 32.5262,
-    lng: -92.6438
-}
-
-const HomePage = () => {
   return (
-    <div className='map'>
-        <LoadScript googleMapsApiKey="AIzaSyD2XWVThP5_5oarPrfZ6dCpQGzpmZ-Hl3s">
-        <GoogleMap
-            mapContainerStyle={mapContainerStyle}
-            center={center}
-            zoom={10}
-        >
-        </GoogleMap>
+    <LoadScript googleMapsApiKey="AIzaSyD2XWVThP5_5oarPrfZ6dCpQGzpmZ-Hl3s">
+      <GoogleMap
+        mapContainerStyle={{ height: '400px', width: '100%' }}
+        center={center}
+        zoom={10}
+      >
+        <Marker position={center} />
+      </GoogleMap>
     </LoadScript>
-    <div className="page">
-        <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-            <h1>HOME</h1>
-            <button type="button">Home button idk</button>
-        </div>
-    </div>
-    </div>  
   );
 };
 
-export default HomePage;
+export default MapComponent;
