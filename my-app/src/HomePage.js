01f32eab26cfd700aebec1de192cee311c424c0a
kalieope <kbp017@email.latech.edu>
2024-10-07 14:38:01 -0500

    Co-authored-by: Colby Willman <cww132@users.noreply.github.com>

--- my-app/src/HomePage.js
+++ my-app/src/HomePage.js
@@ -2,18 +2,24 @@ import React, { useState, useEffect } from 'react';
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 
 const MapComponent = () => {
-  const [center, setCenter] = useState({ lat: 40.7128, lng: -74.0060 }); // Default center (e.g., New York)
+  const [center, setCenter] = useState({ lat: 32.526352, lng: -92.643687 }); // Default center (e.g., New York)
 
   return (
+  <div className="map">
     <LoadScript googleMapsApiKey="AIzaSyD2XWVThP5_5oarPrfZ6dCpQGzpmZ-Hl3s">
       <GoogleMap
         mapContainerStyle={{ height: '400px', width: '100%' }}
         center={center}
-        zoom={10}
+        zoom={800}
       >
         <Marker position={center} />
       </GoogleMap>
     </LoadScript>
+    <div className="button">
+    <button type="button">I am the Map Button</button>
+      </div>
+    </div>
+    
   );
 };
 
