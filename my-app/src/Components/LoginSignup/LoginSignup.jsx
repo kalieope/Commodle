2da403198439ea29e89c096fcd83cecf3a924be1
cww132 <colbywillman@yahoo.com>
2024-10-07 11:55:17 -0500

    Submit does stuff

--- my-app/src/Components/LoginSignup/LoginSignup.jsx
+++ my-app/src/Components/LoginSignup/LoginSignup.jsx
@@ -7,50 +7,67 @@ import password_icon from "../Assets/password.png"
 
 const LoginSignup = () => {
 
-    const [action,setAction] = useState("Login");
+    const [action, setAction] = useState("Login");
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
     const handleSubmit = () => {
         console.log("Name:", name);
-        console.log("Email:",email);
-        console.log("Password",password);
+        console.log("Email:", email);
+        console.log("Password:", password);
     }
 
-    return(
+    return (
         <div className="container">
             <div className="header">
                 <div className="text">{action}</div>
                 <div className="underline"></div>
             </div>
             <div className="inputs">
-                {action==="Login"?<div></div>:<div className="input">
+                {action === "Login" ? <div></div> : 
+                <div className="input">
                     <img src={user_icon} alt="" />
-                    <input type="text" placeholder="Name"/>           
+                    <input 
+                        type="text" 
+                        placeholder="Name"
+                        value={name} 
+                        onChange={(e) => setName(e.target.value)} 
+                    />           
                 </div>}
                 
                 <div className="input">
                     <img src={email_icon} alt="" />
-                    <input type="email" placeholder="Email Address" />           
+                    <input 
+                        type="email" 
+                        placeholder="Email Address"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)} 
+                    />           
                 </div>
                 <div className="input">
                     <img src={password_icon} alt="" />
-                    <input type="password" placeholder="Password" />           
+                    <input 
+                        type="password" 
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)} 
+                    />           
                 </div>
             </div>
-            {action==="Sign Up"?<div></div>:<div className="forgot-password">Lost Password? <span>Click Here!</span></div>}
+            {action === "Sign Up" ? <div></div> : 
+            <div className="forgot-password">Lost Password? <span>Click Here!</span></div>}
+            
             <div className="submit-container">
-                <div className={action==="Login"?"submit gray":"submit"}onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
-                <div className={action==="Sign Up"?"submit gray":"submit"} onClick={()=>{setAction("Login")}}>Login</div>
+                <div className={action === "Login" ? "submit gray" : "submit"} onClick={() => { setAction("Sign Up") }}>Sign Up</div>
+                <div className={action === "Sign Up" ? "submit gray" : "submit"} onClick={() => { setAction("Login") }}>Login</div>
             </div>
+            
             <div className="take-input">
-                <div className={action==="Submit"?"turn gray":"turn"}>Submit</div>
+                <div className={action === "Submit" ? "turn gray" : "turn"} onClick={handleSubmit}>Submit</div>
             </div>
         </div>
     )
 }
 
 export default LoginSignup
-
-
