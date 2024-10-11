ac4074595a3a34778f9378fea0791545d3052295
Jacob Yee <Jacobdy18@hotmail.com>
2024-10-10 22:26:35 -0500

    New features
    Added maps with 2 locations marked with a toilet icon

--- my-app/src/HomePage.js
+++ my-app/src/HomePage.js
@@ -1,30 +1,60 @@
+//pk.eyJ1IjoiamFjb2J5ZWUiLCJhIjoiY20yM2cxeG9qMDViNzJxcHNrMDl0eDhrNSJ9.64obJH6vBfs70H6SL31XHw
+
 import React, { useState, useEffect } from 'react';
-import ReactMapGL from 'react-map-gl';
+import ReactMapGL, { Marker, Popup } from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
+import bathrooms from './markerTest.json';
+import toilet_icon from './toilet.png';
 
 const Map = () => {
-  const [viewPort, setViewport] = useState({
-    latitude: 37.7577,
-    longitude: -122.4376,
-    zoom: 6,
-    width: "100vh",
-    height: "100vh",
 
+    //initial map
+  const [viewPort, setViewport] = useState({
+    latitude: 32.526204537278296,
+    longitude: -92.64369263529309,
+    zoom: 18,
   });
 
 
+  const [selectedToilet, setSelectedToilet] = useState(null);
 
   return (
-    <div style={{ width: "100vh", height: "50vh"}}> 
+    <div style={{ width: "100%", height: "75vh", zIndex: 999}}> 
       <ReactMapGL
       {...viewPort}
-      mapboxAccessToken='pk.eyJ1IjoiamFjb2J5ZWUiLCJhIjoiY20yM2cxeG9qMDViNzJxcHNrMDl0eDhrNSJ9.64obJH6vBfs70H6SL31XHw'
+      mapboxAccessToken='pk.eyJ1IjoiamFjb2J5ZWUiLCJhIjoiY20yM2cxeG9qMDViNzJxcHNrMDl0eDhrNSJ9.64obJH6vBfs70H6SL31XHw
+'
       width='100%'
       height='100%'
       mapStyle={"mapbox://styles/mapbox/standard"}
-      transitionDuration="200"
+      onMove={(evt) => setViewport(evt.viewState)}
+    //Allows to move around map
+      onViewportChange={(viewPort)=>setViewport(viewPort)}
       >
 
+    {bathrooms.features.map((restroom) =>(
+        <Marker key={restroom.properties.ID} 
+            latitude={restroom.properties.Coordinates[1]}
+            longitude={restroom.properties.Coordinates[0]}>
+            
+            <button class="marker-btn" onClick={(e) => {
+                e.preventDefault();
+                setSelectedToilet(restroom);
+            }}>
+                <img src={toilet_icon}
+                    alt='Toilet' />
+            </button>
+        </Marker>
+    ))}
+
+    {selectedToilet ? (
+        <Popup latitude={selectedToilet.properties.Coordinates[1]} 
+                longitude={selectedToilet.properties.Coordinates[0]}
+                onClose={() => setSelectedToilet(null)}>
+            <div>Toilet</div>
+        </Popup>
+    ) : null}
+    
       </ReactMapGL>
     </div>
   );
