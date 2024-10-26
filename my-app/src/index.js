cf0d428f9baaef9cc84c7a189241db7892d4d7ad
kalieope <kbp017@email.latech.edu>
2024-10-26 12:55:53 -0500

    Updated Route

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -31,7 +31,7 @@ const router = createBrowserRouter([
       },
       {
         path: "/about",
-        element: <AboutPage />,
+        element: <ProtectedRoute><AboutPage /></ProtectedRoute>,
       },
       {
         path: "/favorites",
